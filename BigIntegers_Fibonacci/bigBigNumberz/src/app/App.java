c5c1c9a366705b4c10c8c9e83f069d8bccbd5ed6
sadegh-babapour <sam.bp.1988@gmail.com>
2019-10-24 17:04:44 -0600

    with every add, extra space is added to the array, this puts a lot of leading zeros when converting to string, used some obvious stackoverflow remov leading zero to remove the zeros and another pass to add the commas, might be a better way, but to calculate fibonacci of 2020 in less than a second, it is amazing

--- BigIntegers_Fibonacci/bigBigNumberz/src/app/App.java
+++ BigIntegers_Fibonacci/bigBigNumberz/src/app/App.java
@@ -15,16 +15,33 @@ public class App {
         int n = 777;
         String one = "1234567890987654321";
         String two = "1357908642";
-        biginteger.LargeInteger yek = new LargeInteger(one);
+        LargeInteger yek = new LargeInteger(one);
         LargeInteger doo = new LargeInteger(two);
 
 
         System.out.println(yek);
         System.out.println("==============================================");
         System.out.println(yek.add(doo));
+        System.out.println("****************************************");
+        System.out.println(normalFibo(110));
 
         // TO DO:
         // implement multiplication to calculate huge fibonaccizzzzz
         
+    }
+    static LargeInteger normalFibo(int n)
+    {
+        LargeInteger gama = new LargeInteger("0");
+        LargeInteger alpha = new LargeInteger("1");
+        LargeInteger beta = new LargeInteger("1");
+
+        for (int i = 0; i < n-2; i++)
+        {
+            gama = alpha.add(beta);
+            alpha = new LargeInteger(beta);
+            beta = new LargeInteger(gama);
+        }
+
+        return gama;
     }
 }
--- BigIntegers_Fibonacci/bigBigNumberz/src/biginteger/LargeInteger.java
+++ BigIntegers_Fibonacci/bigBigNumberz/src/biginteger/LargeInteger.java
@@ -1,9 +1,15 @@
 package biginteger;
 
+import java.util.Arrays;
+
 public class LargeInteger
 {
     private int [] integers;
 
+    public LargeInteger(LargeInteger input)
+    {
+        this.integers = input.integers;
+    }
     public LargeInteger(String inputNumber)
     {
         int capacity = inputNumber.length();
@@ -18,26 +24,50 @@ public class LargeInteger
     @Override
     public String toString()
     {
-//        String theNumber = "";
-//        for(int i = 0; i < integers.length;i++)
-//        {
-//            theNumber = theNumber.concat(integers[i] + "");
-//        }
-//        return theNumber;
         String theNumber = "";
         int count = 1;
         for(int i = integers.length -1 ; i >=0; i--)
         {
             theNumber = (integers[i] + "").concat(theNumber);
             count++;
+        }
+        // kind of a hack especially when calculating large fibonacci's
+        // for some reason, the array size gets out of hand
+
+        //*
+        // with fibonacci, for each iteration, we are adding an extra space in case the sum over
+        // flows and needs to go to the next section, these add up, hence the leading non zeros
+        //  will come to fix later
+
+        // this is from some stackover flow guy,
+
+
+        char[] array = theNumber.toCharArray();
+        int arrayLength = array.length;
+        int firstNonZeroAt = 0;
+        for(int i=0; i<arrayLength; i++) {
+            if(theNumber.charAt(i) != '0'){
+                theNumber = theNumber.substring(i);
+                break;
+            }
+        }
+        char[] newArr = theNumber.toCharArray();
+        count = 1;
+
+        String finalNumber = "";
+        for(int i = newArr.length -1 ; i >=0; i--) {
+            finalNumber = (newArr[i] + "").concat(finalNumber);
+            count++;
             if (count % 4 == 0)
             {
-                theNumber = ",".concat(theNumber);
+                finalNumber = ",".concat(finalNumber);
                 count =1;
             }
 
         }
-        return theNumber;
+
+
+        return finalNumber;
     }
 
     public LargeInteger add(LargeInteger secondNumber)
